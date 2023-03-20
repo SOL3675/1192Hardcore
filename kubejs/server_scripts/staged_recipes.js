5e8c184cb79d03bba1fb33a92fb6507249415942
SOL3675 <sol3675.at.crafters@gmail.com>
2023-03-20 10:23:03 +0900

    Added staged_recipe.js

---
+++ kubejs/server_scripts/staged_recipes.js
@@ -0,0 +1,84 @@
+// priority: 0
+
+const s1 = "stage1"
+const s2 = "stage2"
+const s3 = "stage3"
+const s4 = "stage4"
+const s5 = "stage5"
+const s6 = "stage6"
+const s7 = "stage7"
+
+ServerEvents.recipes(event => {
+    function StagedShapedCrafting(output, pattern, key, stage) {
+        let staged_recipe = {
+            type: "recipestages:stage",
+            stage: stage,
+            recipe: {
+                type: "minecraft:crafting_shaped",
+                pattern: pattern,
+                key: key,
+                result: output
+            }
+        };
+        event.custom(staged_recipe)
+    }
+
+    function StagedShapelessCrafting(output, inputs, stage) {
+        let ingredients = []
+        inputs.forEach(input => {
+            let item_input = Item.of(input)
+            ingredients.push(Ingredient.of(item_input))
+        })
+        console.info(ingredients)
+        let staged_recipe = {
+            type: "recipestages:stage",
+            stage: stage,
+            shapeless: true,
+            recipe: {
+                type: "minecraft:crafting_shapeless",
+                ingredients: ingredients,
+                result: output
+            }
+        };
+        event.custom(staged_recipe)
+    }
+
+    /* Sample of Adding StagedShapedCrafting
+    StagedShapedCrafting(
+        Item.of('minecraft:furnace'),
+        [
+            'SSS',
+            'S S',
+            'SSS'
+        ],
+        {
+            S: Item.of('minecraft:stone')
+        },
+        "stage1"
+    )
+    */
+
+    /* Sample of Adding StagedShapelessCrafting
+    StagedShapelessCrafting(
+        Item.of('minecraft:furnace'),
+        ['minecraft:diamond_block', 'minecraft:nether_star'],
+        "stage1"
+    )
+    */
+
+    StagedShapedCrafting(
+        Item.of('undergarden:catalyst'),
+        ['D D',' A ','D D'],
+        {
+            A: Item.of('minecraft:iron_block'),
+            D: Item.of('minecraft:diamond_block')
+        },
+        s1
+    )
+
+    StagedShapelessCrafting(
+        Item.of('undergarden:catalyst'),
+        ['minecraft:diamond_block', 'minecraft:nether_star'],
+        s1
+    )
+})
